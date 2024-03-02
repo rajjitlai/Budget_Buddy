9f84aa962e9e75d2a7247cdc3624ff005416ff48
RAJJIT <2.0lairaj@gmail.com>
2024-03-02 22:46:32 +0530

    frontend-01.3-02032024

---
+++ src/utils/cn.js
@@ -0,0 +1,10 @@
+// @ts-nocheck
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+function cn(...inputs) {
+    return twMerge(clsx(inputs));
+}
+
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
