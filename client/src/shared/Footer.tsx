a8f3c67e005a3ea78f6cec063d590f68cd8521b9
RAJJIT <2.0lairaj@gmail.com>
2024-03-03 18:49:57 +0530

    frontend-01.11-03032024

--- client/src/shared/Footer.tsx
+++ client/src/shared/Footer.tsx
@@ -1,23 +1,30 @@
 import { IconBrandGithubFilled, IconBrandYoutubeFilled } from "@tabler/icons-react"
 import rjinstitute from "../assets/rjinstitute.png"
+import { Link } from "react-router-dom"
 
 const Footer = () => {
     return (
         <>
             <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
-            <div className="px-20 w-full flex gap-6 flex-col md:flex-row justify-between">
+            <div className="px-6 md:px-20 w-full flex gap-6 flex-col md:flex-row justify-between">
                 <div>
                     <p className="text-white">Copyright &copy; 2024 BudgetBuddy</p>
                 </div>
                 <ul className="flex gap-2 items-center">
                     <li className="bg-slate-400 rounded-full p-1 cursor-pointer">
-                        <IconBrandGithubFilled />
+                        <Link to="https://github.com/rajjitlai" target="_blank">
+                            <IconBrandGithubFilled />
+                        </Link>
                     </li>
                     <li className="bg-slate-400 rounded-full p-1 cursor-pointer">
-                        <img className="w-6 h-6" src={rjinstitute} alt="developer" />
+                        <Link to="https://rjinstitute.netlify.app/" target="_blank">
+                            <img className="w-6 h-6" src={rjinstitute} alt="developer" />
+                        </Link>
                     </li>
                     <li className="bg-slate-400 rounded-full p-1 cursor-pointer text-[#ee2743]">
-                        <IconBrandYoutubeFilled />
+                        <Link to="https://youtube.com/@rjinstitute.?sub_confirmation=1" target="_blank" >
+                            <IconBrandYoutubeFilled />
+                        </Link>
                     </li>
                 </ul>
             </div>
--- client/src/shared/Topbar.tsx
+++ client/src/shared/Topbar.tsx
@@ -1,15 +1,23 @@
 import { Link } from "react-router-dom";
 import Logo from "../assets/budgetbuddy-logo.png"
+import hb from "../assets/hamburger.svg"
+import { useState } from "react";
 
 const Topbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleNavbar = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <div className="fixed top-0 w-full z-50 shadow-md bg-[#0c0c0c]">
-            <nav className="px-20 py-4 hidden md:flex justify-between w-full text-white items-center">
-                <Link to="/" className="flex items-center decoration-">
-                    <img src={Logo} alt="logo" className='w-16 h-16' />
+            <nav className="px-4 py-4 md:px-20 md:py-4 hidden md:flex justify-between w-full text-white items-center">
+                <Link to="/" className="flex items-center">
+                    <img src={Logo} alt="logo" className="w-16 h-16" />
                     <p>Budget<span className="text-slate-400">Buddy</span></p>
                 </Link>
-                <ul className="flex items-center gap-20 capitalize">
+                <ul className="flex items-center gap-8 capitalize">
                     <li className="hover:text-slate-400">
                         <Link to="/">home</Link>
                     </li>
@@ -25,8 +33,35 @@ const Topbar = () => {
                     <Link className="px-4 py-3 border-0 w-full rounded-md cursor-pointer bg-slate-600" to="/login">Login</Link>
                 </div>
             </nav>
+            <div className="md:hidden">
+                <button onClick={toggleNavbar} className="text-white py-6 px-4 focus:outline-none">
+                    <img src={hb} alt="click" className="w-8 h-8" />
+                </button>
+            </div>
+            {isOpen && (
+                <div className="absolute top-16 right-0 left-0 bg-[#0c0c0c] z-50 shadow-md">
+                    <ul className="flex flex-col items-center gap-6">
+                        <li>
+                            <Link onClick={toggleNavbar} to="/" className="text-white py-4 px-6 block hover:text-slate-400">Home</Link>
+                        </li>
+                        <li>
+                            <Link onClick={toggleNavbar} to="/" className="text-white py-4 px-6 block hover:text-slate-400">About</Link>
+                        </li>
+                        <li>
+                            <Link onClick={toggleNavbar} to="/" className="text-white py-4 px-6 block hover:text-slate-400">Contact</Link>
+                        </li>
+                        <li>
+                            <Link onClick={toggleNavbar} to="/signup" className="text-white py-4 px-6 block hover:text-slate-400">Signup</Link>
+                        </li>
+                        <li>
+                            <Link onClick={toggleNavbar} to="/login" className="text-white py-4 px-6 block hover:text-slate-400">Login</Link>
+                        </li>
+                    </ul>
+                </div>
+            )}
         </div>
     );
 };
+
 
 export default Topbar;
