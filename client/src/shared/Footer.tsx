c4e554f7c63f5dc8e85ea889e54b70b18f9041c9
RAJJIT <2.0lairaj@gmail.com>
2024-03-03 18:05:32 +0530

    frontend-01.10-03032024

--- client/src/shared/Footer.tsx
+++ client/src/shared/Footer.tsx
@@ -1,7 +1,27 @@
+import { IconBrandGithubFilled, IconBrandYoutubeFilled } from "@tabler/icons-react"
+import rjinstitute from "../assets/rjinstitute.png"
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <>
+            <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+            <div className="px-20 w-full flex gap-6 flex-col md:flex-row justify-between">
+                <div>
+                    <p className="text-white">Copyright &copy; 2024 BudgetBuddy</p>
+                </div>
+                <ul className="flex gap-2 items-center">
+                    <li className="bg-slate-400 rounded-full p-1 cursor-pointer">
+                        <IconBrandGithubFilled />
+                    </li>
+                    <li className="bg-slate-400 rounded-full p-1 cursor-pointer">
+                        <img className="w-6 h-6" src={rjinstitute} alt="developer" />
+                    </li>
+                    <li className="bg-slate-400 rounded-full p-1 cursor-pointer text-[#ee2743]">
+                        <IconBrandYoutubeFilled />
+                    </li>
+                </ul>
+            </div>
+        </>
     )
 }
 
--- client/src/shared/Topbar.tsx
+++ client/src/shared/Topbar.tsx
@@ -4,19 +4,25 @@ import Logo from "../assets/budgetbuddy-logo.png"
 const Topbar = () => {
     return (
         <div className="fixed top-0 w-full z-50 shadow-md bg-[#0c0c0c]">
-            <nav className="container flex justify-between w-full text-white">
+            <nav className="px-20 py-4 hidden md:flex justify-between w-full text-white items-center">
                 <Link to="/" className="flex items-center decoration-">
-                    <img src={Logo} alt="logo" className='w-20 h-20' />
-                    <h3>Budget<span className="text-slate-400">Buddy</span></h3>
+                    <img src={Logo} alt="logo" className='w-16 h-16' />
+                    <p>Budget<span className="text-slate-400">Buddy</span></p>
                 </Link>
-                <ul className="flex items-center gap-16 capitalize">
-                    <li><a href="/">home</a></li>
-                    <li><a href="#about">about</a></li>
-                    <li><a href="#contact">contact</a></li>
+                <ul className="flex items-center gap-20 capitalize">
+                    <li className="hover:text-slate-400">
+                        <Link to="/">home</Link>
+                    </li>
+                    <li className="hover:text-slate-400">
+                        <Link to="/">about</Link>
+                    </li>
+                    <li className="hover:text-slate-400">
+                        <Link to="/">contact</Link>
+                    </li>
                 </ul>
-                <div className="bbLoginContainer">
-                    <button className="signupBtn">Signup</button>
-                    <button className="loginBtn">Login</button>
+                <div className="flex justify-around items-center flex-row gap-6">
+                    <Link className="px-4 py-2 border-0 w-full rounded-md cursor-pointer" to="/signup">Signup</Link>
+                    <Link className="px-4 py-3 border-0 w-full rounded-md cursor-pointer bg-slate-600" to="/login">Login</Link>
                 </div>
             </nav>
         </div>
