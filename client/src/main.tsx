1a674eebe92cfcb0a088759561479fd1402c9a0d
RAJJIT <2.0lairaj@gmail.com>
2024-03-05 22:35:36 +0530

    frontend-01.16-05032024

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <main className="w-full h-screen flex justify-center bg-[#121212] items-center dark">
+    <main className="w-full h-screen flex justify-center bg-[#121212] items-center dark overflow-y-auto">
       <App />
     </main>
   </React.StrictMode>,
